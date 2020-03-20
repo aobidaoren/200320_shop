7b0d6a9b214ca4a6a0b7195a0b2016eaf4bb92f4
grace <666666@qq.com>
2020-03-20 10:52:44 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+      <router-view></router-view>
+      <FooterGulde v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+import FooterGulde from './components/FooterGuide/FooterGulde.vue'
+export default {
+    components:{
+        FooterGulde
+    }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+.app
+    width 100%
+    height 100%
+    background-color #f5f5f5
+    position relative
+</style>
---
+++ src/components/FooterGuide/FooterGulde.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="footer_guide">
+      <a href="javascript:;" class="guide_item" :class="{on: '/msite'===$route.path}" 
+            @click="goto('/msite')">
+        <span class="item_icon">
+          <i class="iconfont icon-u"></i>
+        </span>
+        <span>外卖</span>
+      </a>
+      <a href="javascript:;" class="guide_item" :class="{on: '/search'===$route.path}" 
+            @click="goto('/search')">
+        <span class="item _icon">
+          <i class="iconfont icon-ziyuan"></i>
+        </span>
+        <span>搜索</span>
+      </a>
+      <a href="javascript:;" class="guide_item" :class="{on: '/order'===$route.path}" 
+              @click="goto('/order')">
+        <span class="item_icon">
+          <i class="iconfont icon-dingdan1"></i>
+        </span>
+        <span>订单</span>
+      </a>
+      <a href="javascript:;" class="guide_item" :class="{on: '/profile'===$route.path}"
+                    @click="goto('/profile')">
+        <span class="item_icon">
+          <i class="iconfont icon-solid-person"></i>
+        </span>
+        <span>我的</span>
+      </a>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goto(path){
+      this.$router.replace(path)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixins.styl"
+  .footer_guide
+    top border-1px(#e4e4e4)
+    position fixed
+    z-index 100
+    left 0
+    right 0
+    bottom 0
+    background-color #ffffff
+    width 100%
+    height 50px
+    display flex
+    .guide_item
+      display flex
+      flex 1
+      text-align center
+      flex-direction column
+      align-items center
+      margin 5px
+      color #999999
+      text-decoration none
+      
+      span 
+        font-size 12px
+        margin-top 1px
+        margin-bottom 2px
+        .iconfont
+          font-size 22px  
+    .on
+        color #02a774      
+</style>
